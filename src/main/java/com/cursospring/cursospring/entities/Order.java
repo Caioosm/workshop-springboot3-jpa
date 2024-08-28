3eb6e48db2fba56a294de0dbaaaf67c4428fcd6c
caioo <matossousacaio.cms@gmail.com>
2024-08-28 20:31:04 -0300

    order, instant, ISO 8601 part2

--- src/main/java/com/cursospring/cursospring/entities/Order.java
+++ src/main/java/com/cursospring/cursospring/entities/Order.java
@@ -3,6 +3,7 @@ package com.cursospring.cursospring.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +23,7 @@ public class Order implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
