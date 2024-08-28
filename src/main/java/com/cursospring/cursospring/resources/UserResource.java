900da85d4c94ec1b55cd18eac56bce05a6636398
caioo <matossousacaio.cms@gmail.com>
2024-08-28 17:28:03 -0300

    User entity and resource

---
+++ src/main/java/com/cursospring/cursospring/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.cursospring.cursospring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursospring.cursospring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "junio", "junio@gmail.com", "123456789", "123");
+        return ResponseEntity.ok().body(u);
+    }
+}
